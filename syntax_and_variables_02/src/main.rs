a28d0a88dac332e4a9f80f53879f0b2f2d257e1a
bhoglo <5169910+bhoglo@users.noreply.github.com>
2024-04-30 11:13:30 -0500

    Add comments.

--- syntax_and_variables_02/src/main.rs
+++ syntax_and_variables_02/src/main.rs
@@ -2,6 +2,10 @@ use std::env;
 use std::io;
 use slug::slugify;
 
+
+/*
+ * Displays acceptable arguements
+ */
 fn help() {
     println!("------------------------------ \n\
              Usage: ./syntax_and_variables_02 <transformation> \n\
@@ -15,13 +19,16 @@ fn help() {
 }
 
 fn main() {
+    // Collect arguments
     let args: Vec<String> = env::args().collect();
    
+    // Check how many args we have
     if args.len() < 2 || args.is_empty() { 
         help();
         return; // exit code
     }
 
+    // Variables to receive input and transform according to the args
     let transformation: &str = &args[1].to_lowercase();
     let mut user_string = String::new();
     let mut string_mutation = String::new();
@@ -34,9 +41,10 @@ fn main() {
         "uppercase" => string_mutation.push_str(&user_string.to_uppercase()),
         "no-spaces" => string_mutation.push_str(&user_string.replace(" ", "")),
         "slugify" => string_mutation.push_str(&slugify(user_string.clone())),
-        _=> help(),
+        _=> help(), // Display help if transformation doesn't exist
     };
 
+    // Output transformation
     println!("--------------------------- \n\
               Original text: {} \n\
               Transformed text: {} \n\
